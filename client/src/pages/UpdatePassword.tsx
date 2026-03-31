a183f7456b9e8be3816bbf57e956d00d6998feb6
pc_kth <rlaxogudsla2@naver.com>
2026-03-31 23:37:09 +0900

    빌드 에러 수정

--- client/src/pages/UpdatePassword.tsx
+++ client/src/pages/UpdatePassword.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../../../lib/supabase";
 import { KeyRound, CheckCircle2 } from "lucide-react";
 import React, { useState, useEffect } from "react";
 import { useLocation } from "wouter";
